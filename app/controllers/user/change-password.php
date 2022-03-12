e25de8c8688f094aca76189c508da578b387b7db
Ngo Quoc Dat <datlechin@gmail.com>
2022-03-12 17:07:14 +0700

    ui

--- app/controllers/user/change-password.php
+++ app/controllers/user/change-password.php
@@ -15,4 +15,17 @@ if (!isLoggedIn()) {
     exit;
 }
 
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $old_password = cleanInput($_POST['old_password']);
+    $password = cleanInput($_POST['password']);
+    $password_confirm = cleanInput($_POST['password_confirm']);
+
+    if ($old_password === '') {
+        $error = 'Vui lòng nhập mật khẩu cũ';
+    } else if ($password === '') {
+        $error = 'Vui lòng nhập mật khẩu mới';
+    } else if ($password_confirm === '') {
+        $error = 'Vui lòng nhập lại mật khẩu mới';
+    }
+}
 require_once '../../views/user/change-password.php';
--- app/views/user/change-password.php
+++ app/views/user/change-password.php
@@ -4,12 +4,28 @@ require_once '../../views/partials/header.php';
 
 <div class="container py-4">
     <div class="row">
-        <?php
-        require_once '../../views/partials/user-sidebar.php';
-        ?>
-        <div class="col-md-9">
+        <?php require_once '../../views/partials/user-sidebar.php'; ?>
+        <div class="col-md-9 mt-4 mt-md-0">
             <h2 class="text-uppercase fw-bold">Đổi mật khẩu</h2>
-            <form action="/change-password" method="post">
+            <?php if (isset($error)) { ?>
+                <div class="alert alert-danger">
+                    <?php echo $error; ?>
+                </div>
+            <?php } ?>
+            <form action="/user/change-password" method="post">
+                <div class="form-outline mb-3">
+                    <input type="text" id="old_password" name="old_password" class="form-control">
+                    <label for="old_password" class="form-label">Mật khẩu</label>
+                </div>
+                <div class="form-outline mb-3">
+                    <input type="text" id="password" name="password" class="form-control">
+                    <label for="password" class="form-label">Mật khẩu mới</label>
+                </div>
+                <div class="form-outline mb-3">
+                    <input type="text" id="password_confirm" name="password_confirm" class="form-control">
+                    <label for="password_confirm" class="form-label">Xác nhận mật khẩu</label>
+                </div>
+                <button class="btn btn-primary">Đổi mật khẩu</button>
             </form>
         </div>
     </div>
