49d00fa8dbf11af0acb6e28433cf9bb9ee23029c
Ngo Quoc Dat <datlechin@gmail.com>
2022-03-12 13:59:32 +0700

    extract user sidebar

---
+++ app/views/partials/user-sidebar.php
@@ -0,0 +1,8 @@
+<div class="col-md-3">
+    <h5 class="text-uppercase">Menu tài khoản</h5>
+    <div class="list-group mt-4">
+        <a href="/user/profile" class="list-group-item list-group-item-action active" aria-current="true">Thông tin tài khoản</a>
+        <a href="/user/change-password" class="list-group-item list-group-item-action" aria-current="true">Đổi mật khẩu</a>
+        <a href="/user/trans" class="list-group-item list-group-item-action">Lịch sử giao dịch</a>
+    </div>
+</div>
--- app/views/user/profile.php
+++ app/views/user/profile.php
@@ -4,14 +4,9 @@ require_once '../../views/partials/header.php';
 
 <div class="container py-4">
     <div class="row">
-        <div class="col-md-3">
-            <h5 class="text-uppercase">Menu tài khoản</h5>
-            <div class="list-group mt-4">
-                <a href="/user/profile" class="list-group-item list-group-item-action active" aria-current="true">Thông tin tài khoản</a>
-                <a href="/user/change-password" class="list-group-item list-group-item-action" aria-current="true">Đổi mật khẩu</a>
-                <a href="/user/trans" class="list-group-item list-group-item-action">Lịch sử giao dịch</a>
-            </div>
-        </div>
+        <?php
+        require_once '../../views/partials/user-sidebar.php';
+        ?>
         <div class="col-md-9">
             <h2 class="text-uppercase fw-bold">Thông tin tài khoản</h2>
             <table class="table table-striped">
