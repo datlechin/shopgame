9a7cc0cdbf5a4295bf57be0b4db57b12e3640539
Ngo Quoc Dat <datlechin@gmail.com>
2022-03-13 16:24:13 +0700

    better home page

--- app/views/home.php
+++ app/views/home.php
@@ -1,49 +1,50 @@
 <?php
 require_once 'partials/header.php';
 ?>
-    <div id="carousel" class="carousel slide" data-mdb-ride="carousel">
-        <div class="carousel-indicators">
-            <button
-                    type="button"
-                    data-mdb-target="#carouselIndicator"
-                    data-mdb-slide-to="0"
-                    class="active"
-                    aria-current="true"
-                    aria-label="Slide 1"
-            ></button>
-            <button
-                    type="button"
-                    data-mdb-target="#carouselIndicator"
-                    data-mdb-slide-to="1"
-                    aria-label="Slide 2"
-            ></button>
-            <button
-                    type="button"
-                    data-mdb-target="#carouselIndicator"
-                    data-mdb-slide-to="2"
-                    aria-label="Slide 3"
-            ></button>
-        </div>
-        <div class="carousel-inner">
-            <div class="carousel-item" data-mdb-interval="5000">
-                <img src="https://nick.vn/storage/images/lITvp1Ph8r_1623147594.jpg" class="d-block w-100" alt="Camera"/>
-            </div>
-            <div class="carousel-item active" data-mdb-interval="5000">
-                <img src="https://nick.vn/storage/images/XoBF4ldarS_1623147567.jpg" class="d-block w-100" alt="Exotic Fruits"/>
-            </div>
-        </div>
-        <button class="carousel-control-prev" data-mdb-target="#carousel" type="button" data-mdb-slide="prev">
-            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-            <span class="visually-hidden">Trước</span>
-        </button>
-        <button class="carousel-control-next" data-mdb-target="#carousel" type="button" data-mdb-slide="next">
-            <span class="carousel-control-next-icon" aria-hidden="true"></span>
-            <span class="visually-hidden">Sau</span>
-        </button>
-    </div>
+
     <div class="container">
-        <div class="mt-4">
-            <h3 class="text-center text-uppercase fw-bold">Danh mục game</h3>
+        <div id="carousel" class="carousel slide" data-mdb-ride="carousel">
+            <div class="carousel-indicators">
+                <button
+                        type="button"
+                        data-mdb-target="#carouselIndicator"
+                        data-mdb-slide-to="0"
+                        class="active"
+                        aria-current="true"
+                        aria-label="Slide 1"
+                ></button>
+                <button
+                        type="button"
+                        data-mdb-target="#carouselIndicator"
+                        data-mdb-slide-to="1"
+                        aria-label="Slide 2"
+                ></button>
+                <button
+                        type="button"
+                        data-mdb-target="#carouselIndicator"
+                        data-mdb-slide-to="2"
+                        aria-label="Slide 3"
+                ></button>
+            </div>
+            <div class="carousel-inner">
+                <div class="carousel-item" data-mdb-interval="5000">
+                    <img src="https://nick.vn/storage/images/lITvp1Ph8r_1623147594.jpg" class="d-block w-100" alt="Camera"/>
+                </div>
+                <div class="carousel-item active" data-mdb-interval="5000">
+                    <img src="https://nick.vn/storage/images/XoBF4ldarS_1623147567.jpg" class="d-block w-100" alt="Exotic Fruits"/>
+                </div>
+            </div>
+            <button class="carousel-control-prev" data-mdb-target="#carousel" type="button" data-mdb-slide="prev">
+                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                <span class="visually-hidden">Trước</span>
+            </button>
+            <button class="carousel-control-next" data-mdb-target="#carousel" type="button" data-mdb-slide="next">
+                <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                <span class="visually-hidden">Sau</span>
+            </button>
+        </div>
+        <div class="mt-5">
+            <h3 class="text-center text-uppercase fw-bold mb-4">Danh mục game</h3>
             <div class="row">
                 <div class="col-sm-3 mb-3">
                     <div class="card">
--- app/views/partials/header.php
+++ app/views/partials/header.php
@@ -11,7 +11,7 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
     <style>
         body {
-            padding-top: 3.7rem;
+            padding-top: 4.3rem;
         }
     </style>
 </head>
