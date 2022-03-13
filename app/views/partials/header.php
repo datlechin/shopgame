146b0af77da677e3c115c503f0e8e7b8d2276d6d
Ngo Quoc Dat <datlechin@gmail.com>
2022-03-13 15:30:59 +0700

    no min height longer

--- app/views/partials/header.php
+++ app/views/partials/header.php
@@ -11,7 +11,6 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
     <style>
         body {
-            min-height: 75rem;
             padding-top: 3.7rem;
         }
     </style>
