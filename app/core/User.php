69e935fbf43485d9bfb8c7d8b953f4cb41d9eb5c
Ngo Quoc Dat <datlechin@gmail.com>
2022-03-13 17:08:28 +0700

    User comments

--- app/core/User.php
+++ app/core/User.php
@@ -1,5 +1,13 @@
 <?php
 
+/**
+ * Mã nguồn shop bán tài khoản game
+ * @author Ngô Quốc Đạt <datlechin@gmail.com>
+ * @version 0.0.1
+ * @copyright 2022
+ * Vui lòng không xóa các dòng này
+ */
+
 class User
 {
     private object $db;
