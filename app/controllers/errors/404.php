0156bf227a6af92010ce61227b6bd15583285172
Ngo Quoc Dat <datlechin@gmail.com>
2022-03-21 22:13:24 +0700

    404 page

---
+++ app/controllers/errors/404.php
@@ -0,0 +1,18 @@
+<?php
+
+/**
+ * Mã nguồn shop bán tài khoản game
+ * @author Ngô Quốc Đạt <datlechin@gmail.com>
+ * @copyright 2022
+ * Vui lòng không xóa các dòng này
+ */
+
+define('ROOT_PATH', dirname(dirname(dirname(__DIR__))));
+
+require_once ROOT_PATH . '/app/bootstrap.php';
+
+$title = '404 Not Found';
+
+require_once ROOT_PATH . '/app/views/errors/404.php';
+
+header('HTTP/1.0 404 Not Found');
---
+++ app/views/errors/404.php
@@ -0,0 +1,18 @@
+<?php
+require_once ROOT_PATH . '/app/views/partials/header.php';
+?>
+
+<div class="container">
+    <div class="row mt-4">
+        <div class="col-sm-12">
+            <div class="text-center">
+                <h2>404 not found</h2>
+                <p>Không tìm thấy trang bạn đang tìm kiếm!</p>
+            </div>
+        </div>
+    </div>
+</div>
+
+<?php
+require_once ROOT_PATH . '/app/views/partials/footer.php';
+?>
