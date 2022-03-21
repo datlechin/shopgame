3b5c57751727769e4d2142d589830094d7253dba
Ngo Quoc Dat <datlechin@gmail.com>
2022-03-21 22:26:52 +0700

    fix slug

--- app/helpers.php
+++ app/helpers.php
@@ -10,7 +10,8 @@ use JetBrains\PhpStorm\NoReturn;
  */
 
 #[NoReturn]
-function redirect($url) {
+function redirect($url)
+{
     header('Location: ' . $url);
     exit();
 }
@@ -119,12 +120,18 @@ function chargeProvider($provider): string
 
 function slug($string): string
 {
+    $string = strip_tags($string);
     $string = preg_replace('~[^\pL\d]+~u', '-', $string);
+    setlocale(LC_ALL, 'en_US.utf8');
     $string = iconv('utf-8', 'us-ascii//TRANSLIT', $string);
     $string = preg_replace('~[^-\w]+~', '', $string);
     $string = trim($string, '-');
     $string = preg_replace('~-+~', '-', $string);
     $string = strtolower($string);
+
+    if (empty($string)) {
+        return 'n-a';
+    }
 
     return $string;
 }
