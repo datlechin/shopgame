b469f75b14da37114aeaee41347474c27ef69c7a
Ngo Quoc Dat <datlechin@gmail.com>
2022-03-22 20:43:26 +0700

    code backend settings

--- app/controllers/admin/settings.php
+++ app/controllers/admin/settings.php
@@ -15,4 +15,31 @@ if (!$userClass->isAdmin()) {
 
 $title = 'Cài đặt trang web';
 
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $title = $_POST['title'] ? cleanInput($_POST['title']) : setting('title');
+    $description = $_POST['description'] ? cleanInput($_POST['description']) : setting('description');
+    $keywords = $_POST['keywords'] ? cleanInput($_POST['keywords']) : setting('keywords');
+
+    $data = array();
+
+    foreach ($_POST as $key => $value) {
+        if ($key === 'title' || $key === 'description' || $key === 'keywords') {
+            $db->update('settings', ['value' => $value], ['key' => $key]);
+            $data = array(
+                'status' => 'success',
+                'message' => 'Cập nhật thành công'
+            );
+        } else {
+            $data = array(
+                'status' => 'error',
+                'message' => 'Cập nhật thất bại'
+            );
+        }
+    }
+
+    echo json_encode($data);
+    header('application/json');
+    exit();
+}
+
 require_once '../../views/admin/settings.php';
--- app/views/admin/settings.php
+++ app/views/admin/settings.php
@@ -2,40 +2,58 @@
 require_once 'partials/header.php';
 ?>
 
-<section class="content">
-    <div class="container-fluid">
-        <div class="row">
-            <div class="col-md-6">
-                <div class="card">
-                    <div class="card-header">
-                        <h3 class="card-title">Chung</h3>
-                    </div>
-                    <div class="card-body">
-                        <form action="">
-                            <div class="form-group">
-                                <label>Tên trang web:</label>
-                                <input type="text" class="form-control" value="<?php echo setting('title'); ?>">
-                            </div>
-                            <div class="form-group">
-                                <label>Email:</label>
-                                <input type="text" class="form-control" value="<?php echo setting('email'); ?>">
-                            </div>
-                            <div class="form-group">
-                                <label>Từ khóa:</label>
-                                <input type="text" class="form-control" value="<?php echo setting('keywords'); ?>">
-                            </div>
-                            <div class="form-group">
-                                <label>Mô tả:</label>
-                                <textarea class="form-control" rows="3"><?php echo setting('description'); ?></textarea>
-                            </div>
-                            <button type="submit" class="btn btn-primary">Lưu lại</button>
-                        </form>
+    <section class="content">
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-md-6">
+                    <div class="card">
+                        <div class="card-header">
+                            <h3 class="card-title">Chung</h3>
+                        </div>
+                        <div class="card-body">
+                            <form action="/admin/settings" method="post" id="generalSettings">
+                                <div class="form-group">
+                                    <label>Tên trang web:</label>
+                                    <input type="text" name="title" class="form-control" value="<?php echo setting('title'); ?>">
+                                </div>
+                                <div class="form-group">
+                                    <label>Từ khóa:</label>
+                                    <input type="text" name="keywords" class="form-control" value="<?php echo setting('keywords'); ?>">
+                                </div>
+                                <div class="form-group">
+                                    <label>Mô tả:</label>
+                                    <textarea name="description" class="form-control" rows="3"><?php echo setting('description'); ?></textarea>
+                                </div>
+                                <button type="submit" class="btn btn-primary">Lưu lại</button>
+                            </form>
+                        </div>
                     </div>
                 </div>
             </div>
         </div>
-    </div>
-</section>
+    </section>
+
+    <script>
+        $(document).ready(function () {
+            $("#generalSettings").submit(function (e) {
+                e.preventDefault();
+
+                $.ajax({
+                    url: '/admin/settings',
+                    type: 'POST',
+                    data: $(this).serialize(),
+                    success: function (data) {
+                        data = JSON.parse(data);
+                        if (data.status === 'success') {
+                            toastr.success(data.message);
+                        } else {
+                            toastr.error(data.message);
+                        }
+                    }
+                });
+            })
+        });
+    </script>
 
 <?php
 require_once 'partials/footer.php';
