577297ff2f261dc33b7202d4ab31dcce4810ad99
Ngo Quoc Dat <datlechin@gmail.com>
2022-03-24 21:28:34 +0700

    update admin asidebar

--- app/views/admin/partials/aside.php
+++ app/views/admin/partials/aside.php
@@ -1,6 +1,6 @@
 <aside class="main-sidebar sidebar-dark-primary elevation-4">
 
-    <a href="/admin" class="brand-link">
+    <a href="/" class="brand-link">
         <img src="../assets/backend/img/AdminLTELogo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3"
              style="opacity: .8">
         <span class="brand-text font-weight-light">AdminLTE 3</span>
