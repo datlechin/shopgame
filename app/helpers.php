36329a978e99d69460075b616a48c9760b04d973
Ngo Quoc Dat <datlechin@gmail.com>
2022-04-02 21:31:46 +0700

    fix slug helper

--- app/helpers.php
+++ app/helpers.php
@@ -143,22 +143,34 @@ function chargeProvider($provider): string
     }
 }
 
-function slug($string): string
+function slug($str,  $options = array()): string
 {
-    $string = strip_tags($string);
-    $string = preg_replace('~[^\pL\d]+~u', '-', $string);
-    setlocale(LC_ALL, 'en_US.utf8');
-    $string = iconv('utf-8', 'us-ascii//TRANSLIT', $string);
-    $string = preg_replace('~[^-\w]+~', '', $string);
-    $string = trim($string, '-');
-    $string = preg_replace('~-+~', '-', $string);
-    $string = strtolower($string);
-
-    if (empty($string)) {
-        return 'n-a';
+    $str = mb_convert_encoding((string)$str, 'UTF-8', mb_list_encodings());
+    $defaults = array(
+        'delimiter' => '-',
+        'limit' => null,
+        'lowercase' => true,
+        'replacements' => array(),
+        'transliterate' => true,
+    );
+    $options = array_merge($defaults, $options);
+    if ($options['lowercase']) {
+        $str = mb_strtolower($str, 'UTF-8');
     }
+    $char_map = array(
+        // Latin
+        'á' => 'a', 'à' => 'a', 'ả' => 'a', 'ã' => 'a', 'ạ' => 'a', 'ă' => 'a', 'ắ' => 'a', 'ằ' => 'a', 'ẳ' => 'a', 'ẵ' => 'a', 'ặ' => 'a', 'â' => 'a', 'ấ' => 'a', 'ầ' => 'a', 'ẩ' => 'a', 'ẫ' => 'a', 'ậ' => 'a', 'đ' => 'd', 'é' => 'e', 'è' => 'e', 'ẻ' => 'e', 'ẽ' => 'e', 'ẹ' => 'e', 'ê' => 'e', 'ế' => 'e', 'ề' => 'e', 'ể' => 'e', 'ễ' => 'e', 'ệ' => 'e', 'í' => 'i', 'ì' => 'i', 'ỉ' => 'i', 'ĩ' => 'i', 'ị' => 'i', 'ó' => 'o', 'ò' => 'o', 'ỏ' => 'o', 'õ' => 'o', 'ọ' => 'o', 'ô' => 'o', 'ố' => 'o', 'ồ' => 'o', 'ổ' => 'o', 'ỗ' => 'o', 'ộ' => 'o', 'ơ' => 'o', 'ớ' => 'o', 'ờ' => 'o', 'ở' => 'o', 'ỡ' => 'o', 'ợ' => 'o', 'ú' => 'u', 'ù' => 'u', 'ủ' => 'u', 'ũ' => 'u', 'ụ' => 'u', 'ư' => 'u', 'ứ' => 'u', 'ừ' => 'u', 'ử' => 'u', 'ữ' => 'u', 'ự' => 'u', 'ý' => 'y', 'ỳ' => 'y', 'ỷ' => 'y', 'ỹ' => 'y', 'ỵ' => 'y'
+    );
+    $str = preg_replace(array_keys($options['replacements']), $options['replacements'], $str);
+    if ($options['transliterate']) {
+        $str = str_replace(array_keys($char_map), $char_map, $str);
+    }
+    $str = preg_replace('/[^\p{L}\p{Nd}]+/u', $options['delimiter'], $str);
+    $str = preg_replace('/(' . preg_quote($options['delimiter'], '/') . '){2,}/', '$1', $str);
+    $str = mb_substr($str, 0, ($options['limit'] ? $options['limit'] : mb_strlen($str, 'UTF-8')), 'UTF-8');
+    $str = trim($str, $options['delimiter']);
 
-    return $string;
+    return $str;
 }
 
 function setting($key): ?string
